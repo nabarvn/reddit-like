2c3b5e5d0080cd25ef8a292b915889959407255e
Nabarun <nicknabarun@gmail.com>
2023-07-07 21:47:04 +0530

    Export a variable

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
 // 2 to demonstrate infinite scroll, should be higher in production
 export const INFINITE_SCROLL_PAGINATION_RESULTS = 2;
+
+// number of upvotes to determine if the post is going to be cached
+export const CACHE_AFTER_UPVOTES = 1;
