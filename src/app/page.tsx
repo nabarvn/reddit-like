6150f7276d279492780dc0d94975f6e8cf7f0fbf
Nabarun <nicknabarun@gmail.com>
2023-07-07 21:51:57 +0530

    Make db calls for feed posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,86 @@
+import { PostFeed } from "@/components";
 import { buttonVariants } from "@/components/ui/Button";
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
-const Home = () => {
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+const Home = async () => {
+  const session = await getAuthSession();
+
+  const joinedCommunities = await db.subscription.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+    include: {
+      subreddit: true,
+    },
+  });
+
+  const generalFeedPosts = await db.post.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      postVotes: true,
+      author: true,
+      comments: true,
+      subreddit: true,
+    },
+    take: INFINITE_SCROLL_PAGINATION_RESULTS,
+  });
+
+  const customFeedPosts = await db.post.findMany({
+    where: {
+      subreddit: {
+        name: {
+          // return posts whose subreddit name is in this array
+          in: joinedCommunities.map(({ subreddit }) => subreddit.id),
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      postVotes: true,
+      author: true,
+      comments: true,
+      subreddit: true,
+    },
+    take: INFINITE_SCROLL_PAGINATION_RESULTS,
+  });
+
   return (
-    <>
+    <div className='md:container max-w-6xl px-5'>
       <h1 className='font-bold text-3xl md:text-4xl'>Your Feed</h1>
 
       <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
-        {/* feed */}
+        {/* display either custom feed (authenticated view) or general feed (public view) */}
+        {session ? (
+          <PostFeed key='custom' initialPosts={customFeedPosts} />
+        ) : (
+          <PostFeed key='general' initialPosts={generalFeedPosts} />
+        )}
 
         {/* subreddit info */}
         <div className='overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last'>
-          <div className='bg-emerald-100 px-6 py-4'>
+          <div className='bg-emerald-100 px-5 md:px-6 py-4'>
             <p className='font-semibold flex items-center gap-1.5 py-3'>
               <HomeIcon className='h-4 w-4' />
               Home
             </p>
           </div>
 
-          <div className='-my-3 divide-y divide-gray-100 text-sm leading-6 px-6 py-4'>
-            <div className='flex justify-between gap-x-4 py-3'>
+          <div className='-my-3 divide-y divide-gray-100 text-sm leading-6 px-3 md:px-6 py-4'>
+            <div className='flex justify-between gap-x-4 px-2 md:px-0 py-3'>
               <p className='text-zinc-500'>
                 This is where you can stay connected with your favorite
-                communities. Join us to check in!
+                communities. Join to check in!
               </p>
             </div>
 
@@ -38,7 +95,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
