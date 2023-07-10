59732ace0c1eaf264af95b3b6d7d359fd0c2d254
Nabarun <nicknabarun@gmail.com>
2023-07-10 18:57:25 +0530

    Minor styling fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = async () => {
   const session = await getServerSession();
 
   return (
-    <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
+    <div className='fixed top-0 inset-x-0 h-14 xl:h-[60px] bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
       <div className='md:container max-w-7xl h-full mx-auto flex items-center justify-between gap-2 px-5'>
         {/* logo */}
         <Link href='/' className='flex gap-2 items-center'>
