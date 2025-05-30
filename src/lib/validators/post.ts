693a21fa7e6471fb2d601574f6982f833aac9e70
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-05-29 23:32:07 -0400

    Add delete validator for post removal

--- src/lib/validators/post.ts
+++ src/lib/validators/post.ts
@@ -3,10 +3,16 @@ import { z } from "zod";
 export const PostValidator = z.object({
   title: z
     .string()
-    .min(3, { message: "Title must be longer than 3 characters" })
-    .max(128, { message: "Title must be within 128 characters" }),
+    .min(3, { message: "Title must be at least 3 characters long." })
+    .max(128, { message: "Title must be no more than 128 characters long." }),
   subredditId: z.string(),
   content: z.any(),
 });
 
 export type PostCreationRequest = z.infer<typeof PostValidator>;
+
+export const PostDeleteValidator = z.object({
+  postId: z.string(),
+});
+
+export type PostDeletionRequest = z.infer<typeof PostDeleteValidator>;
