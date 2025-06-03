bfa1e13b693f7e51f5eaf98c4491fe32daaab7ae
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-06-03 18:03:50 -0400

    Refine SignUp component layout and link styling

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,13 +1,14 @@
-import { Icons, UserAuthForm } from "@/components";
 import Link from "next/link";
+import { Icons, UserAuthForm } from "@/components";
 
 const SignUp = () => {
   return (
-    <div className='container mx-auto flex flex-col w-full justify-center space-y-6 sm:w-[400px]'>
-      <div className='flex flex-col space-y-2 text-center'>
-        <Icons.logo className='mx-auto h-6 w-6' />
-        <h1 className='text-2xl font-semibold tracking-tight'>Sign up here!</h1>
-        <p className='text-sm max-w-sm mx-auto'>
+    <div className="container mx-auto flex flex-col w-full justify-center space-y-6 sm:w-[400px] px-4">
+      <div className="flex flex-col space-y-2 text-center">
+        <Icons.logo className="mx-auto h-6 w-6" />
+        <h1 className="text-2xl font-semibold tracking-tight">Sign up here!</h1>
+
+        <p className="text-sm max-w-sm mx-auto">
           By continuing, you are setting up a Reddit-Like account and agree to
           our User Agreement and Privacy Policy.
         </p>
@@ -15,11 +16,11 @@ const SignUp = () => {
         {/* sign-in form */}
         <UserAuthForm />
 
-        <p className='text-center text-sm text-zinc-700 px-8'>
+        <p className="text-center text-sm text-zinc-700 px-8">
           Already have an account?{" "}
           <Link
-            href='/sign-in'
-            className='text-sm hover:text-zinc-800 underline underline-offset-4'
+            href="/sign-in"
+            className="text-sm hover:text-zinc-800 underline underline-offset-4"
           >
             Sign In
           </Link>
