a561c8fe49daac6d6bbfc44fc440457b3abf1726
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-06-03 18:05:16 -0400

    Refine layout and styling for the SignIn component

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,13 +1,14 @@
-import { Icons, UserAuthForm } from "@/components";
 import Link from "next/link";
+import { Icons, UserAuthForm } from "@/components";
 
 const SignIn = () => {
   return (
-    <div className='container mx-auto flex flex-col w-full justify-center space-y-6 sm:w-[400px]'>
-      <div className='flex flex-col space-y-2 text-center'>
-        <Icons.logo className='mx-auto h-6 w-6' />
-        <h1 className='text-2xl font-semibold tracking-tight'>Welcome back!</h1>
-        <p className='text-sm max-w-sm mx-auto'>
+    <div className="container mx-auto flex flex-col w-full justify-center space-y-6 sm:w-[400px] px-4">
+      <div className="flex flex-col space-y-2 text-center">
+        <Icons.logo className="mx-auto h-6 w-6" />
+        <h1 className="text-2xl font-semibold tracking-tight">Welcome back!</h1>
+
+        <p className="text-sm max-w-sm mx-auto">
           By continuing, you are setting up a Reddit-Like account and agree to
           our User Agreement and Privacy Policy.
         </p>
@@ -15,11 +16,11 @@ const SignIn = () => {
         {/* sign-in form */}
         <UserAuthForm />
 
-        <p className='text-center text-sm text-zinc-700 px-8'>
+        <p className="text-center text-sm text-zinc-700 px-8">
           New to Reddit-Like?{" "}
           <Link
-            href='/sign-up'
-            className='text-sm hover:text-zinc-800 underline underline-offset-4'
+            href="/sign-up"
+            className="text-sm hover:text-zinc-800 underline underline-offset-4"
           >
             Sign Up!
           </Link>
