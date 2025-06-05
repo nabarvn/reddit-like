1656267adfd8a42500c632a63a38f3e851e45e75
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-06-05 16:02:05 -0400

    Refactor MiniCreatePost component for improved readability and consistency

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { Session } from "next-auth";
-import { usePathname, useRouter } from "next/navigation";
 import { UserAvatar } from "@/components";
 import { Button, Input } from "@/components/ui";
 import { ImageIcon, Link2 } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 
 interface MiniCreatePostProps {
   session: Session | null;
@@ -15,9 +15,9 @@ const MiniCreatePost = ({ session }: MiniCreatePostProps) => {
   const pathname = usePathname();
 
   return (
-    <li className='overflow-hidden rounded-lg bg-white shadow h-fit'>
-      <div className='flex justify-between gap-3 lg:gap-6 h-fit px-3 lg:px-6 py-2 lg:py-4'>
-        <div className='relative h-fit'>
+    <li className="overflow-hidden list-none rounded-lg bg-white shadow h-fit">
+      <div className="flex justify-between gap-3 lg:gap-6 h-fit px-3 lg:px-6 py-2 lg:py-4">
+        <div className="relative h-fit">
           <UserAvatar
             user={{
               name: session?.user.name || null,
@@ -25,29 +25,29 @@ const MiniCreatePost = ({ session }: MiniCreatePostProps) => {
             }}
           />
 
-          <span className='absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white' />
+          <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
         </div>
 
         <Input
           readOnly
           onClick={() => router.push(pathname + "/submit")}
-          placeholder='Create post'
+          placeholder="Create post"
         />
 
         <Button
-          variant='ghost'
+          variant="ghost"
           onClick={() => router.push(pathname + "/submit")}
-          className='px-2 md:px-4 py-1 md:py-2'
+          className="px-2 md:px-4 py-1 md:py-2"
         >
-          <ImageIcon className='text-zinc-600' />
+          <ImageIcon className="text-zinc-600" />
         </Button>
 
         <Button
-          variant='ghost'
+          variant="ghost"
           onClick={() => router.push(pathname + "/submit")}
-          className='px-2 md:px-4 py-1 md:py-2'
+          className="px-2 md:px-4 py-1 md:py-2"
         >
-          <Link2 className='text-zinc-600' />
+          <Link2 className="text-zinc-600" />
         </Button>
       </div>
     </li>
