45048767a748c66a4341ea2863b18498e59d6de4
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-06-09 18:25:36 -0400

    Refactor community creation page for better clarity and consistency

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -4,15 +4,15 @@ import { useState } from "react";
 import axios, { AxiosError } from "axios";
 import { Button, Input } from "@/components/ui";
 import { useRouter } from "next/navigation";
+import { toast } from "@/hooks/use-toast";
 import { useMutation } from "@tanstack/react-query";
 import { CreateSubredditPayload } from "@/lib/validators/subreddit";
-import { toast } from "@/hooks/use-toast";
 import useCustomToast from "@/hooks/use-custom-toast";
 
 const CreateCommunityPage = () => {
+  const router = useRouter();
   const [input, setInput] = useState<string>("");
   const { loginToast } = useCustomToast();
-  const router = useRouter();
 
   const { mutate: createCommunity, isLoading } = useMutation({
     // handles data fetching logic
@@ -30,7 +30,7 @@ const CreateCommunityPage = () => {
       if (err instanceof AxiosError) {
         if (err.response?.status === 409) {
           return toast({
-            title: "Subreddit already exists.",
+            title: "Subreddit already exists",
             description: "Please come up with a different community name.",
             variant: "destructive",
           });
@@ -38,9 +38,8 @@ const CreateCommunityPage = () => {
 
         if (err.response?.status === 422) {
           return toast({
-            title: "Invalid subreddit name.",
-            description:
-              "The length of the community name must be between 3 and 21 characters.",
+            title: "Invalid subreddit name",
+            description: err.response?.data,
             variant: "destructive",
           });
         }
@@ -51,7 +50,7 @@ const CreateCommunityPage = () => {
       }
 
       toast({
-        title: "There was an error.",
+        title: "There was an error",
         description: "Could not create community.",
         variant: "destructive",
       });
@@ -63,35 +62,34 @@ const CreateCommunityPage = () => {
   });
 
   return (
-    <div className='md:container flex items-center h-full max-w-3xl mx-auto px-5'>
-      <div className='relative bg-white w-full h-fit rounded-lg space-y-6 p-4'>
-        <div className='flex justify-between items-center'>
-          <h1 className='text-xl font-semibold'>Create a community</h1>
+    <div className="md:container flex items-center h-full max-w-3xl mx-auto px-5">
+      <div className="relative bg-white w-full h-fit rounded-lg space-y-6 p-4">
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl font-semibold">Create a community</h1>
         </div>
 
-        <hr className='bg-zinc-500 h-px' />
+        <hr className="bg-zinc-500 h-px" />
 
         <div>
-          <p className='text-lg font-medium'>Name</p>
-          <p className='text-xs pb-2'>
-            Community names including capitalization cannot be changed.
-          </p>
+          <p className="text-lg font-medium">Name</p>
 
-          <div className='relative'>
-            <p className='absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400'>
+          <p className="text-xs pb-2">Community names cannot be changed.</p>
+
+          <div className="relative">
+            <p className="absolute text-sm left-0 w-8 inset-y-0 grid place-items-center text-zinc-400">
               r/
             </p>
 
             <Input
               value={input}
               onChange={(e) => setInput(e.target.value)}
-              className='pl-6'
+              className="pl-6"
             />
           </div>
         </div>
 
-        <div className='flex justify-end gap-4'>
-          <Button variant='subtle' onClick={() => router.back()}>
+        <div className="flex justify-end gap-4">
+          <Button variant="subtle" onClick={() => router.back()}>
             Cancel
           </Button>
 
