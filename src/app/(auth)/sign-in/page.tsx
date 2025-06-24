4cd0e9d974b15fa4d55de5a9671c5935f441b867
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-06-24 18:24:57 -0400

    Refine layout and imports in the sign-in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,21 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 import { SignIn } from "@/components";
 import { buttonVariants } from "@/components/ui/Button";
-import { cn } from "@/lib/utils";
 import { ChevronLeft } from "lucide-react";
-import Link from "next/link";
 
 const SignInPage = () => {
   return (
-    <div className='absolute inset-0'>
-      <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
+    <div className="absolute inset-0 px-5">
+      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
         <Link
-          href='/'
+          href="/"
           className={cn(
             buttonVariants({ variant: "ghost" }),
             "self-start -mt-20"
           )}
         >
-          <ChevronLeft className='mr-2 h-4 w-4' />
+          <ChevronLeft className="mr-2 h-4 w-4" />
           Home
         </Link>
 
