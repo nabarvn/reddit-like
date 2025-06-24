933a39ba97b8dbd1323887e7278cbd05ebf0cf2a
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-06-24 18:21:05 -0400

    Refine modal container styling and layout for consistency

--- src/app/@authModal/(.)sign-up/page.tsx
+++ src/app/@authModal/(.)sign-up/page.tsx
@@ -2,10 +2,10 @@ import { CloseModal, SignUp } from "@/components";
 
 const SignUpModal = () => {
   return (
-    <div className='fixed inset-0 bg-zinc-900/20 z-10'>
-      <div className='container flex items-center h-full max-w-lg mx-auto'>
-        <div className='relative bg-white w-full h-fit py-20 px-2 rounded-lg'>
-          <div className='absolute top-4 right-4'>
+    <div className="fixed inset-0 bg-zinc-900/20 z-10">
+      <div className="container flex items-center h-full max-w-lg mx-auto px-4">
+        <div className="relative bg-white w-full h-fit py-20 px-2 rounded-lg">
+          <div className="absolute top-4 right-4">
             <CloseModal />
           </div>
 
