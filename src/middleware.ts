e85f97552aca0c552647594c0e2e16b575858126
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-07-01 18:18:18 -0400

    Enhance middleware with rate limiting and route protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,73 @@
-import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { rateLimiter } from "@/lib/rate-limiter";
+import { withAuth, type NextRequestWithAuth } from "next-auth/middleware";
 
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
+export default withAuth(
+  async function middleware(req: NextRequestWithAuth) {
+    const pathname = req.nextUrl.pathname;
 
-  // user not logged in
-  if (!token) {
-    return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
+    // rate limit API routes
+    if (pathname.startsWith("/api")) {
+      const ip = req.ip ?? "127.0.0.1";
+
+      try {
+        const { success } = await rateLimiter.limit(ip);
+
+        if (!success) {
+          return new NextResponse("Too many requests", { status: 429 });
+        }
+      } catch (error) {
+        return new NextResponse("Internal Server Error", { status: 500 });
+      }
+    }
+
+    // manage route protection
+    const token = req.nextauth.token;
+    const isAuth = !!token;
+    const isAuthPage = req.nextUrl.pathname.startsWith("/sign-in");
+
+    if (isAuthPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+
+      return;
+    }
+
+    const sensitiveRoutes = ["/r/create", "/settings"];
+
+    if (
+      !isAuth &&
+      (sensitiveRoutes.some((route) => pathname.startsWith(route)) ||
+        pathname.match(/\/r\/[^/]+\/submit/))
+    ) {
+      let from = pathname;
+
+      if (req.nextUrl.search) {
+        from += req.nextUrl.search;
+      }
+
+      return NextResponse.redirect(
+        new URL(`/sign-in?from=${encodeURIComponent(from)}`, req.url)
+      );
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // this is so the middleware function above is always called
+        return true;
+      },
+    },
   }
-}
+);
 
-// See "Matching Paths" below to learn more
 export const config = {
-  // middleware will run only on the matched paths
-  matcher: ["/r/:path*/submit", "/r/create"],
+  matcher: [
+    "/api/:path*",
+    "/r/:path*/submit",
+    "/r/create",
+    "/settings",
+    "/sign-in",
+  ],
 };
